74cf29223c64ce4d3ea5c75e236c2d4e5e96362b
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2025-03-09 13:12:04 +0100

    Fix escape to dismiss dropdown

--- src/components/UserDropdown/UserDropdown.tsx
+++ src/components/UserDropdown/UserDropdown.tsx
@@ -1,22 +1,20 @@
-
+import React from "react";
 import style from "./UserDropdown.module.css";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { User } from "react-feather";
-import useToggle from "../../hooks/useToggle";
 import useKeydown from "../../hooks/useKeydown";
 import useClickOutside from "../../hooks/useClickOutside";
-import React from "react";
 
 const UserDropdown = () => {
 	const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
-	const [dropDownVisible, toggleDropdown] = useToggle(false);
+	const [dropDownVisible, setDropdownVisible] = React.useState(false);
 	const dropDownRef = React.useRef<HTMLDivElement>(null);
 
 	// Close dropdown when clicking outside or pressing escape
 	const handleDismiss = React.useCallback(() => {
-		toggleDropdown(false);
-	},[]);
+		setDropdownVisible(false);
+	}, []);
 
 	useKeydown("Escape", handleDismiss);
 	useClickOutside(dropDownRef, handleDismiss, dropDownVisible);
@@ -26,10 +24,13 @@ const UserDropdown = () => {
 		if (!str) return;
 		return str.charAt(0).toUpperCase() + str.slice(1);
 	};
-		
+
 	return (
 		<div className={style.userContainer} ref={dropDownRef}>
-			<button className={style.userButton} onClick={()=>toggleDropdown()}>
+			<button
+				className={style.userButton}
+				onClick={() => setDropdownVisible(prev => !prev)}
+			>
 				{/* Render button content based on auth status */}
 				{isAuthenticated ? (
 					<img src={user?.picture} alt="User image or avatar" />
@@ -41,13 +42,34 @@ const UserDropdown = () => {
 			{/* Show dropdown menu if dropDownVisible (toggle)*/}
 			{dropDownVisible && (
 				<>
-				{/* Show "logged in-dropdown" if authenticated */}
+					{/* Show "logged in-dropdown" if authenticated */}
 					{isAuthenticated ? (
 						<div className={style.userDropdown}>
-							<div className={style.dropdownNickname}>{capitalize(user?.nickname)}</div>							
+							<div className={style.userInfo}>
+								<img
+									src={user?.picture}
+									className={style.userButton}
+									alt="User image or avatar"
+								/>
+								<div className={style.dropdownNickname}>
+									{capitalize(user?.nickname)}
+								</div>
+							</div>
 							<hr />
-							<Link className={style.dropDownLink} onClick={()=>toggleDropdown()} to={"/blogs/1"}>New Blogpost</Link>
-							<Link className={style.dropDownLink} onClick={()=>toggleDropdown()} to={"/blogs/1"}>My posts</Link>
+							<Link
+								className={style.dropDownLink}
+								onClick={() => setDropdownVisible(false)}
+								to={"/blogs/1"}
+							>
+								New Blogpost
+							</Link>
+							<Link
+								className={style.dropDownLink}
+								onClick={() => setDropdownVisible(false)}
+								to={"/blogs/1"}
+							>
+								My posts
+							</Link>
 
 							<button onClick={() => logout()}>Sign Out</button>
 						</div>
