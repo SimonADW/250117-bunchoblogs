e0dc859492391b767da822925c02414680374c9b
SimonADW <112318684+SimonADW@users.noreply.github.com>
2025-03-26 20:09:43 +0100

    Add form placeholders and heading

--- src/components/AddBlogpostForm/AddBlogpostForm.tsx
+++ src/components/AddBlogpostForm/AddBlogpostForm.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form';
 import style from './AddBlogpostForm.module.css';
+
 
 type Inputs = {
 	  title: string;
@@ -17,20 +17,22 @@ const AddBlogpostForm = () => {
 
   return (	
 	<form onSubmit={handleSubmit(onSubmit)} className={style.addBlogpostForm}>
+		<h2>Share the <span>magic</span> here</h2>
+
 		<label htmlFor='title'>Title</label>
-		<input id='title' {...register("title", { required: true })} />
+		<input id='title' placeholder='Enter a catchy blog title...' {...register("title", { required: true })} />
 		{errors.title && <span>This field is required</span>}
 
 		<label htmlFor='author'>Author</label>
-		<input id='author' {...register("author", { required: true })} />
+		<input id='author' placeholder='Your name or pen name' {...register("author", { required: true })} />
 		{errors.author && <span>This field is required</span>}
 
 		<label htmlFor='content'>Content</label>
-		<textarea id='content' {...register("content", { required: true })} />
+		<textarea id='content' placeholder='Compose your blog content' {...register("content", { required: true })} />
 		{errors.content && <span>This field is required</span>}
 
 		<label htmlFor='tags'>Tags</label>
-		<input id='tags' {...register("tags")} />
+		<input id='tags' placeholder='Add tags separated by commas' {...register("tags")} />
 		{errors.tags && <span>This field is required</span>}
 
 		<button type="submit">Submit new post</button>
